655a4ff372413221d201603095c0f89be06ab8d3
kkisic <amareamo.pxhxc@gmail.com>
2020-02-10 16:33:26 +0900

    fix main function

--- sd-local.go
+++ sd-local.go
@@ -4,31 +4,10 @@ import (
 	"log"
 
 	"github.com/screwdriver-cd/sd-local/cmd"
-	"github.com/screwdriver-cd/sd-local/config"
-	"github.com/screwdriver-cd/sd-local/launch"
-	"github.com/screwdriver-cd/sd-local/screwdriver"
 )
 
 func main() {
-	path := "file/path"
-	config, err := config.ReadConfig(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	api, err := screwdriver.New(config.APIURL, config.Token)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	job, err := api.Job("main", "./screwdriver.yaml")
-	if err != nil {
-		log.Fatal(err)
-	}
-	// lauch.Newには API interfaceを渡すように修正したい。
-	l := launch.New(job, config, "main", api.JWT())
-
-	if err := cmd.Execute(config, api, l); err != nil {
+	if err := cmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
 }
