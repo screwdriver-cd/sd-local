ccf7aee34bb794eb7734b74fa1cf3b1d8e9e0c17
kkisic <amareamo.pxhxc@gmail.com>
2020-02-10 14:14:30 +0900

    fix test

--- buildlog/buildlog_test.go
+++ buildlog/buildlog_test.go
@@ -78,6 +78,37 @@ func TestRun(t *testing.T) {
 		assert.Equal(t, strings.Join(expected, ""), writer.String())
 	})
 
+	t.Run("failure by parsing error", func(t *testing.T) {
+		tmpFile, err := ioutil.TempFile("", "")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer tmpFile.Close()
+
+		testInputs := []string{
+			`{"t": 1580198209, "m": "test 1", "n": 0, "s": "main"}` + "\n",
+			`{"t": 1580198222, "m": "test 2", "n": 1, "s": "main"` + "\n",
+		}
+		go write(t, tmpFile.Name(), testInputs)
+
+		parent, cancel := context.WithCancel(context.Background())
+		writer := bytes.NewBuffer(nil)
+		l := log{
+			file:   tmpFile,
+			writer: writer,
+			ctx:    parent,
+			cancel: cancel,
+		}
+
+		go l.Run()
+
+		time.Sleep(3 * time.Second)
+		l.Stop()
+
+		expected := "2020-01-28 16:56:49 +0900 JST: test 1\n"
+
+		assert.Equal(t, expected, writer.String())
+	})
 }
 
 func TestStop(t *testing.T) {
@@ -122,7 +153,6 @@ func TestStop(t *testing.T) {
 		}
 		assert.Equal(t, strings.Join(expected, ""), writer.String())
 	})
-
 }
 
 func TestNew(t *testing.T) {
@@ -168,5 +198,4 @@ func TestNew(t *testing.T) {
 		assert.Equal(t, expected, logger)
 		assert.Equal(t, 0, strings.Index(msg, "failed to open raw build log file: "), fmt.Sprintf("expected error is `failed to open raw build log file: ...`, actual: `%v`", msg))
 	})
-
 }
