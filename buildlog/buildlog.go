723c547d4b2cd1e28aa90416a24814ff10447359
Kazuki Kokufuda <49509459+kkokufud@users.noreply.github.com>
2020-02-17 15:32:52 +0900

    feat: Add buildlog package (#5)
    * add buildlog
    
    * fix: buildlog test
    
    * add test for buildlog.New
    
    * add test
    
    * fix review
    
    * add test for failure New
    
    * fix: error logging
    
    * fix test
    
    * fix test
    
    * fix buildlog
    
    * fix output function
    
    * Revert "fix buildlog"
    
    This reverts commit 29bb885d9d60cd9ec62ed01be48b6656dba6c777.
    This reverts commit 78a1611f428785aa4c00b4cd880c2c87089a9cdc.
    
    * fix: convert timestamp
    
    * fix: time format
    
    * fix: remove writeCloser
    
    * use logrus
    
    Co-authored-by: isic <amareamo.pxhxc@gmail.com>
    Co-authored-by: Mitsuru Takigahira <mysticdoll@mysticdoll.com>
    Co-authored-by: sugarnaoming <sakigithub@gmail.com>

---
+++ buildlog/buildlog.go
@@ -0,0 +1,106 @@
+package buildlog
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+var readInterval time.Duration = 10 * time.Millisecond
+
+// Logger outputs logs
+type Logger interface {
+	Run()
+	Stop()
+}
+
+type log struct {
+	ctx    context.Context
+	file   io.Reader
+	writer io.Writer
+	cancel context.CancelFunc
+}
+
+type logLine struct {
+	Time     int64  `json:"t"`
+	Message  string `json:"m"`
+	Line     int    `json:"n"`
+	StepName string `json:"s"`
+}
+
+// New creates new Logger interface.
+func New(ctx context.Context, filepath string, writer io.Writer) (Logger, error) {
+	log := log{
+		writer: writer,
+	}
+
+	var err error
+	log.file, err = os.OpenFile(filepath, os.O_RDONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		return log, fmt.Errorf("failed to open raw build log file: %w", err)
+	}
+
+	log.ctx, log.cancel = context.WithCancel(ctx)
+
+	return log, nil
+}
+
+func (l log) Stop() {
+	l.cancel()
+	return
+}
+
+func (l log) Run() {
+	reader := bufio.NewReader(l.file)
+
+	for {
+		select {
+		case <-l.ctx.Done():
+			break
+		default:
+			err := l.output(reader)
+			if err != nil {
+				logrus.Errorf("failed to run logger: %w\n", err)
+				logrus.Info("But build is not stopping")
+				l.cancel()
+			}
+		}
+		time.Sleep(readInterval)
+	}
+}
+
+func (l log) output(reader *bufio.Reader) error {
+	line, _, err := reader.ReadLine()
+	if err != nil {
+		if err != io.EOF {
+			return fmt.Errorf("failed to read logfile: %w", err)
+		}
+		return nil
+	}
+
+	parsedLog, err := parse(line)
+	if err != nil {
+		return fmt.Errorf("failed to output log: %w", err)
+	}
+
+	fmt.Fprintln(l.writer, parsedLog)
+	return nil
+}
+
+func parse(rawLog []byte) (string, error) {
+	ll := &logLine{}
+	err := json.Unmarshal(rawLog, ll)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse raw log: %w", err)
+	}
+
+	ISOTime := time.Unix(0, ll.Time*int64(time.Millisecond)).Format("2006-01-02 03:04:05")
+
+	return fmt.Sprintf("%s %s", ISOTime, ll.Message), nil
+}
---
+++ buildlog/buildlog_test.go
@@ -0,0 +1,189 @@
+package buildlog
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	intervalTime = 500
+	timeRegEx    = `[\d]{4}-[\d]{2}-[\d]{2} [\d]{2}:[\d]{2}:[\d]{2}`
+)
+
+var (
+	testInputs = []string{
+		`{"t": 1581662022394, "m": "test 1", "n": 0, "s": "main"}` + "\n",
+		`{"t": 1581662022395, "m": "test 2", "n": 1, "s": "main"}` + "\n",
+	}
+)
+
+func write(tb testing.TB, filepath string, inputs []string) {
+	tb.Helper()
+
+	for _, input := range inputs {
+		file, err := os.OpenFile(filepath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			tb.Fatal(err)
+		}
+
+		_, err = file.Write([]byte(input))
+		if err != nil {
+			tb.Fatal(err)
+		}
+
+		err = file.Close()
+		if err != nil {
+			tb.Fatal(err)
+		}
+	}
+}
+
+func TestRun(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		tmpFile, err := ioutil.TempFile("", "")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer tmpFile.Close()
+
+		go write(t, tmpFile.Name(), testInputs)
+
+		parent, cancel := context.WithCancel(context.Background())
+		writer := bytes.NewBuffer(nil)
+		l := log{
+			file:   tmpFile,
+			writer: writer,
+			ctx:    parent,
+			cancel: cancel,
+		}
+
+		go l.Run()
+
+		time.Sleep(intervalTime * time.Millisecond)
+		l.Stop()
+
+		expected := regexp.MustCompile(timeRegEx + " test 1\n" + timeRegEx + " test 2\n")
+		assert.Regexpf(t, expected, writer.String(), "time format is invalid")
+	})
+
+	t.Run("failure by parsing error", func(t *testing.T) {
+		tmpFile, err := ioutil.TempFile("", "")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer tmpFile.Close()
+
+		testInvalidInputs := []string{
+			`{"t": 1581662022394, "m": "test 1", "n": 0, "s": "main"}` + "\n",
+			`{` + "\n",
+		}
+		go write(t, tmpFile.Name(), testInvalidInputs)
+
+		parent, cancel := context.WithCancel(context.Background())
+		writer := bytes.NewBuffer(nil)
+		l := log{
+			file:   tmpFile,
+			writer: writer,
+			ctx:    parent,
+			cancel: cancel,
+		}
+
+		go l.Run()
+
+		time.Sleep(intervalTime * time.Millisecond)
+		l.Stop()
+
+		expected := regexp.MustCompile(timeRegEx + " test 1\n")
+		assert.Regexpf(t, expected, writer.String(), "time format is invalid")
+	})
+}
+
+func TestStop(t *testing.T) {
+	t.Run("success, confirm not to write log after stopped", func(t *testing.T) {
+		tmpFile, err := ioutil.TempFile("", "")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer tmpFile.Close()
+
+		testInputsNotWritten := []string{
+			`{test}` + "\n",
+		}
+
+		go write(t, tmpFile.Name(), testInputs)
+
+		parent, cancel := context.WithCancel(context.Background())
+		writer := bytes.NewBuffer(nil)
+		l := log{
+			file:   tmpFile,
+			writer: writer,
+			ctx:    parent,
+			cancel: cancel,
+		}
+
+		go l.Run()
+
+		time.Sleep(intervalTime * time.Millisecond)
+		l.Stop()
+
+		go write(t, tmpFile.Name(), testInputsNotWritten)
+		time.Sleep(intervalTime * time.Millisecond)
+
+		expected := regexp.MustCompile(timeRegEx + " test 1\n" + timeRegEx + " test 2\n")
+		assert.Regexpf(t, expected, writer.String(), "time format is invalid")
+	})
+}
+
+func TestNew(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		tmpFile, err := ioutil.TempFile("", "")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer tmpFile.Close()
+
+		writer := bytes.NewBuffer(nil)
+
+		logger, err := New(context.Background(), tmpFile.Name(), writer)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		log, ok := logger.(log)
+		if !ok {
+			t.Fatal("Failed to convert Logger to log")
+		}
+
+		file, ok := log.file.(*os.File)
+		if !ok {
+			t.Fatal("Failed to convert Reader to File")
+		}
+
+		assert.Equal(t, tmpFile.Name(), file.Name())
+		assert.Equal(t, writer, log.writer)
+	})
+
+	t.Run("failure", func(t *testing.T) {
+		writer := bytes.NewBuffer(nil)
+
+		logger, err := New(context.Background(), "/", writer)
+
+		expected := log{
+			writer: writer,
+			file:   (*os.File)(nil),
+		}
+
+		msg := err.Error()
+		assert.Equal(t, expected, logger)
+		assert.Equal(t, 0, strings.Index(msg, "failed to open raw build log file: "), fmt.Sprintf("expected error is `failed to open raw build log file: ...`, actual: `%v`", msg))
+	})
+}
