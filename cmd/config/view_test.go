161ab17fc33e96d81a77e84c252cee1977b4c40c
sugarnaoming <sakigithub@gmail.com>
2020-02-21 18:38:21 +0900

    fix: remove duplicate test

--- cmd/config/view_test.go
+++ cmd/config/view_test.go
@@ -2,56 +2,10 @@ package config
 
 import (
 	"bytes"
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
-	"github.com/screwdriver-cd/sd-local/config"
 )
-
-func Example_newConfigViewCmd() {
-	cmd := NewConfigCmd()
-	cmd.SetArgs([]string{"view"})
-	cmd.SetOut(os.Stdout)
-
-	c := config.Config{
-		APIURL:   "api.screwdriver.com",
-		StoreURL: "store.screwdriver.com",
-		Token:    "token",
-		Launcher: config.Launcher{
-			Version: "latest",
-			Image:   "screwdrivercd/launcher",
-		},
-	}
-	configNew = func(configPath string) (config.Config, error) {
-		return c, nil
-	}
-
-	cmd.Execute()
-	// Output:
-	// KEY               VALUE
-	// api-url           api.screwdriver.com
-	// store-url         store.screwdriver.com
-	// token             token
-	// launcher-version  latest
-	// launcher-image    screwdrivercd/launcher
-}
-
-func Example_newConfigViewCmd_local() {
-	cmd := NewConfigCmd()
-	cmd.SetArgs([]string{"view", "--local"})
-	// buf := bytes.NewBuffer(nil)
-	cmd.SetOut(os.Stdout)
-	cmd.Execute()
-	// Output:
-	// KEY               VALUE
-	// api-url           local.api.screwdriver.com
-	// store-url         local.store.screwdriver.com
-	// token             local.token
-	// launcher-version  latest
-	// launcher-image    screwdrivercd/launcher
-}
 
 func TestViewCmd(t *testing.T) {
 	fp := filePath
